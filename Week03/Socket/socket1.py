e27554cdd28a4f46a433595b1e85c977a36c4de3
Ronen Gilead-Raz <ronengi@gmail.com>
2017-07-15 21:59:27 +0300

    Add decode() to convert binary array to a string.

--- Week03/Socket/socket1.py
+++ Week03/Socket/socket1.py
@@ -3,20 +3,15 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# mysock.connect(('www.py4inf.com', 80))
 mysock.connect(('data.pr4e.org', 80))
-
-msg = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'
-# msg = 'GET /code/romeo.txt HTTP/1.1\r\nHOST: www.py4inf.com\r\n\r\n'
-sent = mysock.send(msg.encode())
-
-print(sent)
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
 
 while True:
     data = mysock.recv(512)
     if len(data) < 1:
         break
-    print(data)
+    print(data.decode())
 
 mysock.close()
 
