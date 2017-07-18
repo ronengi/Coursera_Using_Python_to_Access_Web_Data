7b37ca67e2d575201bbaac8c067f82adf2eeae9a
Ronen Gilead-Raz <ronengi@gmail.com>
2017-07-19 02:01:33 +0300

    Add week 5: xml

---
+++ Week05/parse_xml_01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import xml.etree.ElementTree as ET
+
+data = '''<person>
+    <name>Chuck</name>
+    <phone type="intl">
+        +1 734 303 4456
+    </phone>
+    <email hide="yes"/>
+</person>'''
+
+tree = ET.fromstring(data)      # potentially exception
+print('Name:', tree.find('name').text)
+print('Attr:', tree.find('email').get('hide'))
+
+
