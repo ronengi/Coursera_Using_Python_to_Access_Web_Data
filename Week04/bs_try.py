38cd236c3c4e97ce90f33af48dcea0b68ce0cac8
Ronen Gilead-Raz <ronengi@gmail.com>
2017-07-19 02:00:51 +0300

    Add BeautifulSoup HTML parser example

---
+++ Week04/bs_try.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+
+
